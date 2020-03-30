4ae4530f45124f178fa097feda3265cb4f983815
ryuki999 <ryuki999_1234@yahoo.co.jp>
2020-03-31 01:10:35 +0900

    housework_appのsettings.pyとurl.pyに'house'を追加

--- housework_app/housework_app/settings.py
+++ housework_app/housework_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "house",
 ]
 
 MIDDLEWARE = [
@@ -116,5 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
